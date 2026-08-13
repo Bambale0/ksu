94511459d5f5cb95a2fc333279d715363d52c209
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-14 00:13:38 +0300

    Start partner application migration

---
+++ alembic/versions/0023_partner_applications.py
@@ -0,0 +1,14 @@
+"""partner applications"""
+
+revision = "0023_partner_applications"
+down_revision = "0022_batch_generation_commands"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
