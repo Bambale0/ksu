68d5175b1f9663eb030bef77551de31ffe52d063
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-13 22:59:17 +0300

    Start reference library migration

---
+++ alembic/versions/0018_reference_library_presets.py
@@ -0,0 +1,14 @@
+"""reference library and presets"""
+
+revision = "0018_reference_library_presets"
+down_revision = "0017_prompt_tools"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
