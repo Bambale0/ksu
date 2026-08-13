9f6560ef21acf05ccdce2fce8f32e8f1007fe170
Bambale0 <83171474+Bambale0@users.noreply.github.com>
2026-08-13 23:21:10 +0300

    Start batch generation migration

---
+++ alembic/versions/0020_batch_generation.py
@@ -0,0 +1,14 @@
+"""durable batch generation"""
+
+revision = "0020_batch_generation"
+down_revision = "0019_preset_billing_seconds"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
